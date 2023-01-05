0824fba1c5ac261767017c306f39fb9c340e4991
Lenson-Lee <dmstjs7437@naver.com>
2023-01-05 17:55:55 +0900

    CRUD 완성, 작성시간 계산기능 추가

--- pages/api/post/update.js
+++ pages/api/post/update.js
@@ -4,11 +4,16 @@ const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   const { id } = JSON.parse(req.body);
-  const deletedArticle = await prisma.user.delete({
+  const updateUser = await prisma.user.update({
     where: {
       id: id,
     },
+    data: {
+      name: "김수한무거북이와두루미",
+      // email: "kim@turtle.swan",-> unique를 왜 넣어 바보야
+      content: "삼천갑자 동방삭 치치카포 사리사리",
+    },
   });
-  console.log(deletedArticle);
-  res.status(200).json({ message: "삭제 끝났어용" });
+  console.log(updateUser);
+  res.status(200).json({ message: "수정 끝났어용" });
 }
